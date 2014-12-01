a2cf38a05f557dc1e1ca61206e8fd52e57924c31
James Newman <jamesmnewman1@gmail.com>
2014-12-01 03:09:04 -0800

    set up

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* scrolling buttons on nav
+for this, if you add '.page-scroll a' it will transition to the top of whatever #id you give it */
+
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+}); 
